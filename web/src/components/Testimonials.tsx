0ef92de2a043f5e34200bf1381917d43a98d3078
gus <53922692+guswithahoodie@users.noreply.github.com>
2026-01-03 12:32:29 -0600

    Update Testimonials.tsx

--- web/src/components/Testimonials.tsx
+++ web/src/components/Testimonials.tsx
@@ -24,7 +24,7 @@ export default function Testimonials() {
     <section className="bg-white py-12">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-16">
-          Lo que dicen nuestras clientas
+          Lo que dicen nuestros clientes
         </h2>
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
